b8e68e53c538cf5f30057ea3b5f401cfcaaea5e8
vifavero <victoriafavero009@gmail.com>
2025-11-24 21:37:26 -0300

    :recycle: refactor: Implementation of new routes

--- src/app/templates/router.tsx
+++ src/app/templates/router.tsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import { CardLogin } from "../organisms/CardLogin";
 import { RegistrationPage } from "./RegistrationPage";
 import { ListKids } from "./ListKids";
+import { Profile } from "./Profile";
+import { Register } from "../organisms/Register";
+import { ListAttendance } from "./ListAttendance";
+import { Scanner } from "../organisms/qrCode";
+import { ScannerQrCode } from "./Scanner";
 
 export const router = createBrowserRouter([
   {
@@ -15,5 +20,21 @@ export const router = createBrowserRouter([
   {
     path: "/list",
     element: <ListKids />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/attendance/presentes",
+    element: <ListAttendance />,
+  },
+  {
+    path: "/qrcode",
+    element: <ScannerQrCode />,
   },
 ]);
