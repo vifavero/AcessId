9d8e6c55389ff07a4eb131addfd3a113a0205667
vifavero <victoriafavero009@gmail.com>
2025-11-24 21:46:39 -0300

    :sparkles: feat: Implementation of the presence list and association modal of individual wristbands

---
+++ src/app/templates/DescriptionCard.tsx
@@ -0,0 +1,36 @@
+import { Card } from "@/components/ui/card";
+import { useContext } from "react";
+import { FormContext } from "./provider";
+
+export function DescriptionCard() {
+  const { nameKids, nameParents, description } = useContext(FormContext);
+
+  const fields = [
+    { label: "Nome:", value: nameKids },
+    { label: "Nome do responsável:", value: nameParents },
+    { label: "Outras informações:", value: description },
+  ];
+
+  return (
+    <div className="flex items-center justify-center gap-10 p-5 bg-secondary rounded-2xl ">
+      <img
+        src="src/assets/images/kid.jpg"
+        className="rounded-full w-20 h-20 md:w-32 md:h-32 object-cover"
+      />
+
+      <Card className="flex flex-col gap-4 p-6 bg-orange-100 ">
+        <div className="text-white text-sm space-y-2 ">
+          {fields.map(({ label, value }) => (
+            <div
+              key={label}
+              className="flex gap-2 wrap-break-words whitespace-break-spaces"
+            >
+              <h2 className="font-semibold">{label}</h2>
+              <p className="font-medium">{value}</p>
+            </div>
+          ))}
+        </div>
+      </Card>
+    </div>
+  );
+}
---
+++ src/app/templates/ListAttendance.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import { SelectButton } from "../molecules/selectButton";
+import { KidsCard } from "../organisms/KidsCard";
+
+export function ListAttendance() {
+  interface Kid {
+    id: number;
+    nameKids: string;
+    nameParents: string;
+    atipica: boolean;
+    description: string;
+  }
+  const [kids, setKids] = useState<Kid[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const kidsAttendance = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const res = await fetch(`http://localhost:3333/attendance/presentes`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error("Erro ao buscar crianças");
+        }
+
+        const data = await res.json();
+        setKids(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    kidsAttendance();
+  }, []);
+
+  while (loading)
+    return (
+      <div className="flex flex-col min-h-screen w-screen bg-[url('src/assets/images/pattern.png')] bg-cover bg-center">
+        <div className="bg-green-200 flex p-2 justify-center items-center">
+          <p>Sincronizando dados...</p>
+        </div>
+      </div>
+    );
+
+  if (kids.length === 0)
+    return (
+      <div className="flex flex-col min-h-screen w-screen bg-[url('src/assets/images/pattern.png')] bg-cover bg-center">
+        <div className="flex flex-1 flex-col items-center justify-center gap-10">
+          <div className="flex items-center bg-blue-dark text-white rounded-2xl p-5 transition duration-500 hover:scale-125 hover:shadow-sm hover:shadow-blue-200">
+            <p className="text-center text-2xl font-medium">
+              Nenhuma criança presente
+            </p>
+          </div>
+        </div>
+
+        <footer className="w-full bg-secondary py-4 border-t flex justify-center">
+          <SelectButton />
+        </footer>
+      </div>
+    );
+
+  return (
+    <>
+      <div className="flex flex-col min-h-screen w-screen bg-[url('src/assets/images/pattern.png')] bg-cover bg-center">
+        <div className="flex flex-col flex-grow items-center justify-center">
+          <div className="w-full md:w-1/2 bg-secondary p-5 rounded-2xl">
+            {kids.map((kid) => (
+              <KidsCard key={kid.id} kid={kid} />
+            ))}
+          </div>
+        </div>
+
+        <footer className="w-full bg-secondary py-4 flex justify-center">
+          <SelectButton />
+        </footer>
+      </div>
+    </>
+  );
+}
